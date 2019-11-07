7abe0cfe8dd061a9da3fb71e9a9fdeb488721c9b
Shaun <37434846+ShaunLWM@users.noreply.github.com>
2019-11-08 01:30:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ curl \
 -X POST \
 --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${PB_TOKEN}" \
---data-binary '{"type": "note", "title": $PB_TITLE, "body": $PB_TEXT}'
+--data-binary '{"type": "note", "title": $PB_TITLE, "body": $PB_TEXT}' \
 "https://api.pushbullet.com/v2/pushes"
