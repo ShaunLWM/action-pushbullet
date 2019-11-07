779bcb3f33f7f33b372c5f357260212123f6c460
Shaun <37434846+ShaunLWM@users.noreply.github.com>
2019-11-08 01:01:11 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+curl \
+--silent --output /dev/null \
+--header "Content-Type: application/json"
+--header "Authorization: ${PB_TOKEN}"
+--data-urlencode "type=note" \
+--data-urlencode "title=$PB_TITLE" \
+--data-urlencode "text=$PB_TEXT" \
+"https://api.pushbullet.com/v2/pushes"
